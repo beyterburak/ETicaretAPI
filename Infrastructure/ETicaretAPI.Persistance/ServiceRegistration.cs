39bee1091554efe789d82ebcc56395e645a5402b
BURAK\beyte <beyterburak@gmail.com>
2023-10-13 19:05:29 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistance.Contexts;
+using ETicaretAPI.Persistance.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+        }
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+        private readonly IOrderReadRepository _orderReadRepository;
+        private readonly IOrderWriteRepository _orderWriteRepository;
+        private readonly ICustomerReadRepository _customerReadRepository;
+        private readonly ICustomerWriteRepository _customerWriteRepository;
+
+        public ProductController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository, IOrderReadRepository orderReadRepository, IOrderWriteRepository orderWriteRepository, ICustomerReadRepository customerReadRepository, ICustomerWriteRepository customerWriteRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+            _orderReadRepository = orderReadRepository;
+            _orderWriteRepository = orderWriteRepository;
+            _customerReadRepository = customerReadRepository;
+            _customerWriteRepository = customerWriteRepository;
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+            Product product = await _productReadRepository.GetByIdAsync("13ee8eff-5307-4cf7-b2cd-e76ca4b1ac5b");
+            product.Stock = 2;
+            await _productWriteRepository.SaveAsync();
+        }
+    }
+}
