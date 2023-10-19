66dae213b55f2e719ff0741820b83c68c16a51e6
BURAK\beyte <beyterburak@gmail.com>
2023-10-19 20:01:07 +0300

    added: Validation

---
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,38 @@
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator() 
+        { 
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Lütfen ürün adını boş geçmeyiniz.")
+                .MaximumLength(150)
+                .MinimumLength(3)
+                    .WithMessage("Lütfen ürün adını 3 ile 150 karakter arasında giriniz.");
+
+            RuleFor(x => x.Stock)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Lütfen stok bilgisini boş geçmeyiniz.")
+                .Must(s => s >= 0)
+                    .WithMessage("Stok bilgisi negatif olamaz!");
+
+            RuleFor(x => x.Price)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Lütfen fiyat bilgisini boş geçmeyiniz.")
+                .Must(p => p >= 0)
+                    .WithMessage("Fiyat bilgisi negatif olamaz!");
+        }
+    }
+}
